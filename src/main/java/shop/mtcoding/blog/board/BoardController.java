603b5286b3b4256ea7c22477d4edf6711d109119
hyeon <lhyeonnit@gmail.com>
2024-03-20 17:43:37 +0900

    주석

--- src/main/java/shop/mtcoding/blog/board/BoardController.java
+++ src/main/java/shop/mtcoding/blog/board/BoardController.java
@@ -18,18 +18,21 @@ public class BoardController {
     private final BoardService boardService;
     private final HttpSession session;
 
+    // 메인화면(글 목록)
     @GetMapping("/")
     public ResponseEntity<?> main() {
         List<BoardResponse.MainDTO> requestDTO = boardService.findAll();
         return ResponseEntity.ok(new ApiUtil(requestDTO));
     }
 
+    // 글 조회(업데이트전에)
     @GetMapping("/api/boards/{id}")
     public ResponseEntity<?> findOne(@PathVariable Integer id) {
         Board board = boardService.updateForm(id);
         return ResponseEntity.ok(new ApiUtil(board));
     }
 
+    // 글 상세보기
     @GetMapping("/api/boards/{id}/detail")
     public ResponseEntity<?> detail(@PathVariable Integer id) {
         User sessionUser = (User) session.getAttribute("sessionUser");
@@ -37,6 +40,7 @@ public class BoardController {
         return ResponseEntity.ok(new ApiUtil(board));
     }
 
+    // 글쓰기
     @PostMapping("/api/boards")
     public ResponseEntity<?> save(@RequestBody BoardRequest.SaveDTO requestDTO) {
         User sessionUser = (User) session.getAttribute("sessionUser");
@@ -45,6 +49,7 @@ public class BoardController {
         return ResponseEntity.ok(new ApiUtil(board));
     }
 
+    // 글수정
     @PutMapping("/api/boards/{id}")
     public ResponseEntity<?> findById(@PathVariable Integer id, @RequestBody BoardRequest.UpdateDTO requestDTO) {
         User sessionUser = (User) session.getAttribute("sessionUser");
@@ -56,6 +61,7 @@ public class BoardController {
     // SSR(서버 사이드 렌더링)은 DTO 를 만들지 않아도 된다. 필요한 데이터만 렌더링해서 클라이언트에게
     // 전달할 것이니까.
 
+    // 글삭제
     @DeleteMapping("/api/boards/{id}")
     public ResponseEntity<?> delete(@PathVariable Integer id) {
         User sessionUser = (User) session.getAttribute("sessionUser");
